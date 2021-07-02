89f6e11e525ea28539600fc42af2901a60a01903
acotrina <acotrina@espol.edu.ec>
2021-07-02 02:44:14 -0500

    Sección noticias

---
+++ js/slickNoticia.js
@@ -0,0 +1,38 @@
+$(function(){
+
+$('.for_slick_slider').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  nextArrow:$('.next'),
+  prevArrow: $('.prev'),
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+});
