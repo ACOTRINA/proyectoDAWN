5d81a8fc28de9fc6946d69baaf302454b90099e3
ricdasal <56646823+ricdasal@users.noreply.github.com>
2021-07-15 23:52:31 -0500

    agregando buscador interno

---
+++ Primer Parcial/js/buscador.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    
+
+});
+
+
+document.getElementById('buscador2').onclick = function(){
+    buscador1();  
+};
+
+
+
+
+function buscador1(){
+    let noticias =  document.getElementsByClassName('items');
+
+    let tBuscador = document.getElementsByTagName('input');
+    let texto =  tBuscador[1].value;
+    let text1 = texto.toUpperCase();
+    
+
+    let html ='';
+    for(noticia of noticias){
+        
+        let n = noticia.getElementsByClassName('titulo')[0].textContent;
+        
+        
+        if(n.toUpperCase().includes(text1)){
+            let divinfo =  noticia.getElementsByClassName('informacion')[0];
+            let informacion =  divinfo.getElementsByTagName('p')[0].textContent;
+
+            let notIndividual = noticia.getElementsByTagName('h3')[0].textContent;
+
+            let imagen = noticia.getElementsByTagName('img')[0];
+            let altimg = imagen.getAttribute('alt');
+            let atImg =  imagen.getAttribute('src');
+            
+
+
+            //console.log(n);
+            
+
+            html += `
+            <div class="items">
+ 
+            <div class="card noticiaIndividual">
+              <div class="card-header">
+                <h3>${notIndividual}</h3>
+              </div>
+    
+              <div class="informacion">
+                <p class="titulo">${n}</p>
+              </div>
+              <img src="${atImg}" alt="${altimg}">
+              <div class="informacion">
+                <p>${informacion}</p>
+              </div>
+            </div>
+        
+        </div>`
+            
+
+        }
+
+
+    }
+    document.getElementById('item-container').innerHTML = html;
+
+
+}
