13c0dcafa9b276ea34722b432396f1fac103936a
ricdasal <56646823+ricdasal@users.noreply.github.com>
2021-09-08 14:07:06 -0500

    parte del apartado de productos del frontend

--- Segundo Parcial/front-Saldur/src/app/productos/productos.component.ts
+++ Segundo Parcial/front-Saldur/src/app/productos/productos.component.ts
@@ -8,12 +8,28 @@ import { AppComponent } from '../app.component';
 })
 export class ProductosComponent implements OnInit {
   
+  productos:Array<any> = [];
   constructor(app:AppComponent) {
     app.showNav = false;
     app.showFoot = false; 
    }
 
   ngOnInit(): void {
+    this.cargarProductos();
+  }
+
+  cargarProductos(): void {
+    fetch("http://localhost:3001/productosactuales")
+    .then(response => response.json())
+    .then(productos => {
+     
+      for(let producto of productos){
+        this.productos = producto;
+      }
+    })
+    .catch(console.error);
+
+  }
   }
 
 }
